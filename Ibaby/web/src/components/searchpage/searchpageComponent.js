a33a322afc723ef5e047db402eafa38ea11f6d42
Eragon00 <185676324@qq.com>
2018-02-06 08:46:38 +0800

    shouye

--- Ibaby/web/src/components/searchpage/searchpageComponent.js
+++ Ibaby/web/src/components/searchpage/searchpageComponent.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react'
+import {hashHistory} from 'react-router'
+import './searchpage.scss'
 
 class searchpageComponent extends Component{
+    _back(){
+        hashHistory.go(-1);
+    }
+
     render(){
         return (
-            <div>
-
+            <div className="searchpage">
+                <div className="searchpageheader">
+                    <i className="iconfont icon-shangyiye1" onClick={this._back.bind(this)}></i>
+                    <div>
+                        <span>商品</span><span>育儿</span><span>亲子</span>
+                    </div>
+                </div>
+                <div className="searchcontent">
+                    <div>
+                        <div className="searchinput">
+                            <i className="iconfont icon-search"></i>
+                            <input type="text" placeholder="纸尿裤199减50"/>
+                        </div>
+                        <div className="sousuo">搜索</div>
+                    </div>
+                </div>
             </div>
         )
     }
