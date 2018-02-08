1ea92dd2e35c7f535b65ea3b8ea1b63a2ce97581
mylmj <937947471@qq.com>
2018-02-08 21:17:40 +0800

    orders

--- Ibaby/web/src/router/index.js
+++ Ibaby/web/src/router/index.js
@@ -15,6 +15,7 @@ import settlementComponent from '../components/settlement/settlementComponent.js
 import ListComponent from '../components/list/listComponent'
 import cartComponent from '../components/cart/cartComponent.js'
 import addressComponent from '../components/address/addressComponent.js'
+import paymentComponent from '../components/payment/paymentComponent.js'
 
 const routes = (
     <Route>
@@ -31,6 +32,7 @@ const routes = (
         <Route path="cart" component={cartComponent}/>
         <Route path="settlement" component={settlementComponent}/>
         <Route path="address" component={addressComponent}/>
+        <Route path="payment" component={paymentComponent}/>
     </Route>
 )
 
