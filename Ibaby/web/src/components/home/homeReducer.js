d9e2ea22ca46617b756ede0985dc5ccc1f02318f
lhh33168 <3316876256@qq.com>
2018-02-09 10:07:52 +0800

    zhenghe

--- Ibaby/web/src/components/home/homeReducer.js
+++ Ibaby/web/src/components/home/homeReducer.js
@@ -4,16 +4,15 @@ import * as tabsConstants from './homeConstants.js'
 export default function homeReducer(state = {}, action){
     let newState = JSON.parse(JSON.stringify(state));
     switch(action.type){
-        case (ajaxConstants.AJAX_REQUESTING || tabsConstants.TABS_RQUESTING):
+        case (tabsConstants.BANNER_REQUESTING || tabsConstants.TABS_RQUESTING):
             newState.status = 0;
             break;
-        case ajaxConstants.AJAX_REQUESTED:
-           try{
-                newState.status = 1;
-                newState.result = action.result.data.results;
-            }catch(error){}
+        case tabsConstants.BANNER_RQUESTED:
+            newState.status = 1;
+            newState.result = action.result.data.results;
+            console.log(newState.result)
             break;
-        case (ajaxConstants.AJAX_REQUESTERROR || tabsConstants.TABS_RQUESTERROR):
+        case (tabsConstants.BANNER_REQUESTERROR || tabsConstants.TABS_RQUESTERROR):
             newState.status = -1;
             break;
         case tabsConstants.TABS_RQUESTED:
